dfe10077c14c5a50197ee9146587e847f1c8bd9a
@mmogesdesigns <mmogesdesigns@gmail.com>
2024-04-29 18:41:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// TASK #1
+let name = "Mary"
+let age = 23
+let grade = 12
+let attendanceStatus = true
+
+console.log("Student Information:")
+console.log("Name: "+ name)
+console.log("Age: " + age)
+console.log("Grade: "+ grade )
+console.log("Attendance Status: "+attendanceStatus )
+
+// TASK #2
+let x = 2
+let y = 4
+
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:",quotient);
+
+x += 1;
+console.log("Updated x (+1)", x)
+
+y *= 4;
+console.log("Updated y (*4)", x)
+
+console.log("Is num1 equal to num2?", x === y);
+console.log("Is num1 greater than num2?", x > y);
+console.log("Is num1 not equal to num2?", x !== y);
+
+let isPositive = true;
+let isEven = true;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
